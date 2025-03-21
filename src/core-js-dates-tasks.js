73029134cad201367dbacb07ac384595d2affcc6
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-03-21 20:21:27 +0300

    feat: complete isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -155,8 +155,15 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const inputDate = new Date(date);
+  if (
+    Date.parse(period.start) <= inputDate &&
+    Date.parse(period.end) >= inputDate
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
