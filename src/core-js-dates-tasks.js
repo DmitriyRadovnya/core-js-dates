de3bcd4f2656310f80fab834b5cb9cc734b51da1
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-03-21 20:44:19 +0300

    feat: complete formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -177,8 +177,22 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const obj = new Date(date);
+  const year = obj.getUTCFullYear();
+  const month = obj.getUTCMonth() + 1;
+  const day = obj.getUTCDate();
+
+  const min = String(obj.getUTCMinutes()).padStart(2, 0);
+  const sec = String(obj.getUTCSeconds()).padStart(2, 0);
+
+  let hour = obj.getUTCHours();
+  const amPm = hour < 12 ? 'AM' : 'PM';
+
+  hour %= 12;
+  hour = hour || 12;
+
+  return `${month}/${day}/${year}, ${hour}:${min}:${sec} ${amPm}`;
 }
 
 /**
